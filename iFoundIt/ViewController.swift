d5c8cc1c8cd84d9c16edc121adb98f4ed3ce06cb
Manuel <naruto06@mail.ru>
2016-04-08 15:11:38 +0300

    Initial Commit

---
+++ iFoundIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iFoundIt
+//
+//  Created by Мануэль on 08.04.16.
+//  Copyright © 2016 Мануэль. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
