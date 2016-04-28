1f92ea12d423ab6f45a70a4a35b72576c6a8879b
Manuel <naruto06@mail.ru>
2016-04-28 13:16:04 +0300

    Added: Error handling

--- iFoundIt/SearchViewController.swift
+++ iFoundIt/SearchViewController.swift
@@ -75,29 +75,34 @@ extension SearchViewController: UISearchBarDelegate
         
         searchResults = [SearchResult]()
         
-        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+        let url = urlWithSearchText(searchBar.text!)
         
-        dispatch_async(queue) {
+        let session = NSURLSession.sharedSession()
         
-        let url = self.urlWithSearchText(searchBar.text!)
-        
-        if let jsonString = self.performStoreRequestWithURL(url),
-            let dict = self.parseJSON(jsonString) {
+        let dataTask = session.dataTaskWithURL(url) {
+            data, response, error in
             
-            self.searchResults = self.parseDictionary(dict)
-            
-            self.searchResults.sortInPlace(<)
-            
+            if let error = error { print("Failure! \(error)") }
+            else if let httpResponse = response as? NSHTTPURLResponse where httpResponse.statusCode == 200 {
+                guard let data = data, dictionary = self.parseJSON(data) else { return }
+                
+                self.searchResults = self.parseDictionary(dictionary)
+                self.searchResults.sortInPlace(<)
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.isLoading = false
+                    self.tableView.reloadData()
+                }
+                return
+            }
             dispatch_async(dispatch_get_main_queue()) {
-                self.isLoading = false
+                self.hasSearched = false
+                self.isLoading   = false
                 self.tableView.reloadData()
+                self.showNetworkError()
             }
-            
-            return
-            }
-            
-            dispatch_async(dispatch_get_main_queue()) { self.showNetworkError() }
         }
+        dataTask.resume()
     }
 }
 
@@ -171,26 +176,13 @@ extension SearchViewController
     func urlWithSearchText(searchText: String) -> NSURL {
         
         let encodedText = searchText.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
-        let urlString   = String(format: "https://itunes.apple.com/search?term=%@", encodedText)
+        let urlString   = String(format: "https://itunes.apple.com/search?term=%@&limit200", encodedText)
         let url         = NSURL(string: urlString)
         
         return url!
     }
     
-    func performStoreRequestWithURL(url: NSURL) -> String? {
-        
-        do {
-            return try String(contentsOfURL: url, encoding: NSUTF8StringEncoding)
-        }
-        catch {
-            print("Download Error: \(error)")
-            return nil
-        }
-    }
-    
-    func parseJSON(jsonString: String) -> [String: AnyObject]? {
-        
-        guard let data = jsonString.dataUsingEncoding(NSUTF8StringEncoding) else { return nil }
+    func parseJSON(data: NSData) -> [String: AnyObject]? {
         
         do {
             return try NSJSONSerialization.JSONObjectWithData(data, options: []) as? [String: AnyObject]
