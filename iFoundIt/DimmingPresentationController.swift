fb3188f4e779dd928ead618b1a1299c643775aac
Manuel <naruto06@mail.ru>
2016-05-04 18:18:24 +0300

    Added: New animation controllers
    New Animations

--- iFoundIt/DimmingPresentationController.swift
+++ iFoundIt/DimmingPresentationController.swift
@@ -13,9 +13,26 @@ class DimmingPresentationController: UIPresentationController
     lazy var dimmingView = GradienView(frame: CGRect.zero)
     
     override func presentationTransitionWillBegin() {
+        
         dimmingView.frame = containerView!.bounds
         containerView!.insertSubview(dimmingView, atIndex: 0)
+        
+        dimmingView.alpha = 0
+        
+        guard let transitionCoordinator = presentedViewController.transitionCoordinator() else { return }
+        
+        transitionCoordinator.animateAlongsideTransition({ _ in
+            self.dimmingView.alpha = 1
+            }, completion: nil)
     }
+    
+    override func dismissalTransitionWillBegin() {
+        guard let transitionCoordinator = presentedViewController.transitionCoordinator() else { return }
+        
+        transitionCoordinator.animateAlongsideTransition({ _ in
+            self.dimmingView.alpha = 0
+            }, completion: nil)
+    }    
     
     override func shouldRemovePresentersView() -> Bool {
         return false
